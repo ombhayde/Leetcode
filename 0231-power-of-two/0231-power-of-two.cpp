74cddcceb0af565b18625650595f87b271987cef
ombhayde <135888892+ombhayde@users.noreply.github.com>
2025-01-10 21:10:32 +0530

    Time: 0 ms (100%), Space: 7.4 MB (97.38%) - LeetHub

--- 0231-power-of-two/0231-power-of-two.cpp
+++ 0231-power-of-two/0231-power-of-two.cpp
@@ -1,6 +1,16 @@
 class Solution {
 public:
     bool isPowerOfTwo(int n) {
-        
+        int i=2;
+        if (n==1||n==2){
+            return true;
+        }
+        while((pow(2,i))<=n){
+            if ((n==(int(pow(2,i))))){
+                return true;
+            }
+            i++;
+        }
+        return false;
     }
 };
